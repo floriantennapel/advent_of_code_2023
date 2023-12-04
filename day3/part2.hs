51a49927b160f6211b4b384759ad598d2e09d45b
Florian ten Napel <tennapel.f@gmail.com>
2023-12-04 18:19:59 +0100

    Added part2

---
+++ day3/part2.hs
@@ -0,0 +1,72 @@
+import Data.Maybe (isJust, fromMaybe)
+import Data.List (nub)
+
+--safe version of !!
+get :: Int -> [a] -> Maybe a
+get i xs | i >= length xs || i < 0 = Nothing
+         | otherwise = Just $ xs !! i
+
+
+
+
+
+
+
+--input is a string where the first element is known to be a numeric value
+parseInt :: String -> (Int, Int) --(value, number of digits)
+parseInt line = (read number, length number) 
+    where 
+        number = takeWhile(`elem` ['0'..'9']) line
+
+-- lists all numbers on line, including starting index
+-- "..58" -> [(Nothing, 0), (Nothing, 1), (Just 58, 2), (Just 58, 2)]
+numsOnLine :: String -> [(Maybe Int, Int)]
+numsOnLine = numsOnLine' 0
+
+numsOnLine' :: Int -> String -> [(Maybe Int, Int)]
+numsOnLine' _ []     = []
+numsOnLine' i (c:cs) | elem c ['0'..'9'] = [(Just val, i) | _ <- [1..len]] ++
+                       (numsOnLine' (i+len) (drop len (c:cs)))
+                     | otherwise = (Nothing, i) : (numsOnLine' (i+1) cs)
+    where
+        (val, len) = parseInt (c:cs)
+
+--returns a list of all elements surrounding given position
+getAdjacent :: Int -> Int -> [[(Maybe Int, Int)]] -> [(Maybe Int, Int)]
+getAdjacent row col nums = concat $
+                           map (take 3 . drop (col - 1) . fromMaybe []) $
+                           filter (isJust) $
+                           get (row-1) nums : get row nums : [get (row+1) nums]
+
+--given list of surrounding elements, either returns
+--the product of the two surrounding numbers, or 0 if 
+--less than two unique numbers are in the list
+gearProduct :: [(Maybe Int, Int)] -> Int
+gearProduct =  gearNum . 
+               map (fromMaybe 0 . fst) .
+               nub .
+               filter (isJust . fst)
+    where
+        gearNum surrounding | length surrounding == 2 = product surrounding
+                            | otherwise = 0
+
+
+
+
+
+
+--given an engine schematic, returns the sum of all gear-ratios
+sumGears :: String -> Int
+sumGears input = sumGears' 0 0 (lines input) nums
+    where
+        nums = map numsOnLine $ lines input 
+
+sumGears' :: Int -> Int -> [String] -> [[(Maybe Int, Int)]] -> Int
+sumGears' _ _ [] _ = 0
+sumGears' row col ((c:cs):ls) nums 
+    | c == '*' = (gearProduct $ getAdjacent row col nums) + next 
+    | otherwise = next
+    where
+        next = case cs of
+                 [] -> sumGears' (row+1) 0 ls nums 
+                 _  -> sumGears' row (col+1) (cs:ls) nums
