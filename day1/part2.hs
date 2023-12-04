a496849df3f6521235c3e428ec8576f3e10712f8
Florian ten Napel <tennapel.f@gmail.com>
2023-12-04 21:45:31 +0100

    Update part2.hs

--- day1/part2.hs
+++ day1/part2.hs
@@ -1,30 +1,20 @@
-import Data.List (isInfixOf)
+import Data.Char (isDigit)
+import Data.Text (replace, pack, unpack)
 
-calibrate :: String -> Int 
+--a bit verbose, but it works very well
+calibrate :: String -> Int
 calibrate = sum .
-            map (\cs -> read (firstNum "" cs : [lastNum cs ""]) :: Int) .
-            lines
-
-numInString :: String -> Char
-numInString cs | any (`isInfixOf` cs) ["one", "1"]   = '1' 
-               | any (`isInfixOf` cs) ["two", "2"]   = '2'
-               | any (`isInfixOf` cs) ["three", "3"] = '3'
-               | any (`isInfixOf` cs) ["four", "4"]  = '4'
-               | any (`isInfixOf` cs) ["five", "5"]  = '5'
-               | any (`isInfixOf` cs) ["six", "6"]   = '6'
-               | any (`isInfixOf` cs) ["seven", "7"] = '7'
-               | any (`isInfixOf` cs) ["eight", "8"] = '8'
-               | any (`isInfixOf` cs) ["nine", "9"]  = '9'
-               | otherwise = '0'
-
-firstNum :: String -> String -> Char
-firstNum fsts []   = numInString fsts
-firstNum fsts rest = case numInString fsts of
-                         '0' -> firstNum (fsts ++ [head rest]) (tail rest)
-                         num -> num 
-
-lastNum :: String -> String -> Char
-lastNum [] lasts   = numInString lasts 
-lastNum rest lasts = case numInString lasts of  
-                         '0' -> lastNum (init rest) ((last rest) : lasts)
-                         num -> num 
+            map (\cs -> read ((head cs) : [last cs]) :: Int) .
+            map (filter isDigit) .
+            lines .
+            unpack .
+            replace (pack "one") (pack "o1e")  .
+            replace (pack "two") (pack "t2o")  .
+            replace (pack "three") (pack "t3e") .
+            replace (pack "four") (pack "4") .
+            replace (pack "five") (pack "5e") .
+            replace (pack "six") (pack "6") .
+            replace (pack "seven") (pack "7n") .
+            replace (pack "eight") (pack "e8t") .
+            replace (pack "nine") (pack "n9e") .
+            pack
