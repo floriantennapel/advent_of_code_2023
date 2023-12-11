8a2039f37f2dc759ef47882970741690e5a5b77d
Florian ten Napel <tennapel.f@gmail.com>
2023-12-11 13:57:40 +0100

    Add files via upload

---
+++ day11/part1.hs
@@ -0,0 +1,33 @@
+import Data.List (transpose, sort)
+
+main :: IO ()
+main = do
+    input <- readFile "inputData.txt"
+    let diffs = getDiffs $ expand $ lines input 
+    putStrLn $ show $ (sumDiffs $ fst diffs) + 
+                      (sumDiffs $ snd diffs)
+               
+               
+
+expand :: [[Char]] -> [[Char]]
+expand = transpose .
+         expandRows .
+         transpose . 
+         expandRows
+    where
+        expandRows = concat . 
+                     map (\r -> if all (=='.') r 
+                                   then [r,r] 
+                                   else [r])
+
+getDiffs :: [[Char]] -> ([Int], [Int])
+getDiffs xs = (\poss -> ((map fst poss),(sort $ map snd poss))) $
+              map (\(_,i,j) -> (i,j)) $
+              filter (\(x,_,_) -> x == '#') $
+              concat $
+              [[(x,i,j) | (x,j) <- zip row [0..]] | (row, i) <- zip xs [0..]]
+
+-- finds the difference between all combinations of pairs and sums them
+sumDiffs :: [Int] -> Int
+sumDiffs ns = (sum $ zipWith (*) ns [0..]) -
+              (sum $ zipWith (*) (reverse ns) [0..])
