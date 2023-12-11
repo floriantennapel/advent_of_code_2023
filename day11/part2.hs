b4a03c28d0b2645205d7e901e54f72838796d0ca
Florian ten Napel <tennapel.f@gmail.com>
2023-12-11 15:21:38 +0100

    cleaned up part2.hs

--- day11/part2.hs
+++ day11/part2.hs
@@ -3,19 +3,17 @@ import Data.List (transpose, sort)
 main :: IO ()
 main = do
     input <- readFile "inputData.txt"
-    let gals = getGalaxies $ getIndices $ lines input 
+    let gals = getGalaxies $ lines input 
     putStrLn $ show $ (sumDiffs $ fst gals) + 
                       (sumDiffs $ snd gals)
 
-getGalaxies :: [(Char, Integer, Integer)] -> ([Integer], [Integer])
-getGalaxies = (\poss -> ((map fst poss),(sort $ map snd poss))) .
-              map (\(_,i,j) -> (i,j)) . 
-              filter (\(x,_,_) -> x == '#')
-
-getIndices :: [[Char]] -> [(Char, Integer, Integer)]
-getIndices css = concat $
-                 [[(x,i,j) | (x, j) <- zip cs (getRowIndices 0 $ transpose css)] | 
-                  (cs, i) <- zip css (getRowIndices 0 css)] 
+getGalaxies :: [[Char]] -> ([Integer], [Integer])
+getGalaxies css = (\poss -> ((map fst poss),(sort $ map snd poss))) $
+                  map (\(_,i,j) -> (i,j)) $
+                  filter (\(x,_,_) -> x == '#') $
+                  concat $
+                  [[(x,i,j) | (x, j) <- zip cs (getRowIndices 0 $ transpose css)] | 
+                   (cs, i) <- zip css (getRowIndices 0 css)]
 
 getRowIndices :: Integer -> [[Char]] -> [Integer] 
 getRowIndices _ []  = []
