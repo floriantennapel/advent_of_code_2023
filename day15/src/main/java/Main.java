9de1f00c65ac1093c8d7460da28037be0e26f569
Florian ten Napel <tennapel.f@gmail.com>
2023-12-15 16:48:38 +0100

    Update Main.java

--- day15/src/main/java/Main.java
+++ day15/src/main/java/Main.java
@@ -4,6 +4,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
