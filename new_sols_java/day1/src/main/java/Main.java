dc4aa8b7b78cb0d58e79ec2231d27f2409f2f34e
Florian ten Napel <tennapel.f@gmail.com>
2024-01-30 18:46:00 +0100

    solved day1

---
+++ new_sols_java/day1/src/main/java/Main.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
+    List<String> lines = new ArrayList<>(reader.lines().toList());
+
+    System.out.println(Part1.sumLines(lines));
+    System.out.println(Part2.sumLines(lines));
+  }
+}
---
+++ new_sols_java/day1/src/main/java/Part2.java
@@ -0,0 +1,54 @@
+import java.util.List;
+import java.util.Map;
+
+public class Part2 {
+  private static final Map<String, Integer> numbers = Map.of(
+      "zero", 0, "one", 1, "two", 2, "three", 3, "four", 4,
+      "five", 5, "six", 6, "seven", 7, "eight", 8, "nine", 9
+  );
+
+  public static int sumLines(List<String> lines) throws RuntimeException {
+    return lines.stream().
+        map(Part2::calibrationValue).
+        reduce(0, Integer::sum);
+  }
+
+  private static int calibrationValue(String line) throws RuntimeException {
+    int first = getFirstOrLastNum(line, true);
+    int last = getFirstOrLastNum(line, false);
+
+    return Integer.parseInt(String.format("%d%d", first, last));
+
+  }
+
+  /**
+   * @param line
+   * @param first look for the first or last number
+   * @return the first or last number on the line, -1 if none are found
+   */
+  private static int getFirstOrLastNum(String line, boolean first) throws RuntimeException {
+    char[] chars = line.toCharArray();
+
+    for (int i = first ? 0 : chars.length - 1;
+         first ? i < chars.length : i >= 0;
+         i += first ? 1 : -1) {
+
+      if (Character.isDigit(chars[i])) {
+        return Character.getNumericValue(chars[i]);
+      }
+
+      StringBuilder collectedChars = new StringBuilder();
+
+      for (int j = i; j <= i + 5 && j < chars.length; j++) { // longest number-word is 5 characters
+        collectedChars.append(chars[j]);
+
+        if (numbers.containsKey(collectedChars.toString())) {
+          return numbers.get(collectedChars.toString());
+        }
+      }
+    }
+
+    throw new RuntimeException("no number found on line");
+  }
+
+}
